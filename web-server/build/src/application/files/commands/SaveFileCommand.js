f641a2384a8673cdbbb2445dc6de3a49f608cb4c
gianmt-06 <moragian6@gmail.com>
2024-11-05 16:15:21 -0500

    sync

--- web-server/build/src/application/files/commands/SaveFileCommand.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.SaveFileCommand = void 0;
-class SaveFileCommand {
-}
-exports.SaveFileCommand = SaveFileCommand;
-//# sourceMappingURL=SaveFileCommand.js.map
--- web-server/build/src/util/FileRoute.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getFileRoute = void 0;
-const getFileRoute = (file, requestURI) => {
-    return (file.getMimetype().split("/")[0] === "image") ? `${requestURI}/files/image?hash=${file.getFingerprint()}` : "";
-};
-exports.getFileRoute = getFileRoute;
-//# sourceMappingURL=FileRoute.js.map
